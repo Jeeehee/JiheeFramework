5f3d80412dbfa29a54de232196ea47a0ba090150
jiheeHwang <jhhwang@autocrypt.io>
2023-09-14 14:23:34 +0900

    add: Test Code

--- Tests/JiheePackageTests/JiheePackageTests.swift
+++ Tests/JiheePackageTests/JiheePackageTests.swift
@@ -1,7 +1,11 @@
 import XCTest
+import JiheeFramework
+
 @testable import JiheePackage
 
 final class JiheePackageTests: XCTestCase {
     func testExample() throws {
+        let text = "Hello, World!"
+        XCTAssertEqual(makePaddingLabel(text: text).backgroundColor, getPaddingLabel(text: text).backgroundColor)
     }
 }
